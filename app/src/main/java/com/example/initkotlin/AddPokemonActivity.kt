429f2871cf084335fdcf184275b4ea7bba1914bb
Rachel Lima <rachel.l@samsung.com>
2020-06-14 21:15:41 -0400

    Add layout add pokemon
    Signed-off-by: Rachel Lima <rachel.l@samsung.com>

---
+++ app/src/main/java/com/example/initkotlin/AddPokemonActivity.kt
@@ -0,0 +1,11 @@
+package com.example.initkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddPokemonActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_pokemon)
+    }
+}
